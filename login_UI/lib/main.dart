d167017a825277dbe3d9b889f5b6c4c69cf5ae99
Keshav Sharma <keshav2111101@akgec.ac.in>
2022-10-16 01:12:41 +0530

    learning futurebuilder

--- login_UI/lib/main.dart
+++ login_UI/lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
-
 import 'package:flutter/material.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
       title: "Blockchain",
@@ -48,50 +46,69 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
       // future: Firebase.initializeApp(),
       appBar: AppBar(title: const Text('Blockchain')),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          TextFormField(
-            controller: _email,
-            enableSuggestions: false,
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            decoration: const InputDecoration(
-              labelText: 'Email',
-              contentPadding: EdgeInsets.only(left: 10),
-              hintText: "Email",
-              border: UnderlineInputBorder(
-                borderSide: BorderSide(),
-              ),
-            ),
-          ),
-          TextFormField(
-            controller: _passowrd,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            decoration: const InputDecoration(
-              hintText: "Password",
-              contentPadding: EdgeInsets.only(left: 10),
-              border: UnderlineInputBorder(borderSide: BorderSide()),
-              labelText: 'Password',
-            ),
-          ),
-          TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _passowrd.text;
-                await Firebase.initializeApp(options: defaultFirebase);
-
-                final UserCredential = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: email, password: password);
-                print(UserCredential);
-              },
-              child: const Text('Login'))
-        ],
+      body: FutureBuilder(
+        builder: (context, snapshot) {
+          // Future:
+          Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          );
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      contentPadding: EdgeInsets.only(left: 10),
+                      hintText: "Email",
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                  ),
+                  TextFormField(
+                    controller: _passowrd,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: const InputDecoration(
+                      hintText: "Password",
+                      contentPadding: EdgeInsets.only(left: 10),
+                      border: UnderlineInputBorder(borderSide: BorderSide()),
+                      labelText: 'Password',
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _passowrd.text;
+                        final userCredential = await FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+                        print(userCredential);
+                      },
+                      child: const Text('Login'))
+                ],
+              );
+            case ConnectionState.active:
+              return const Text('loading always one...');
+            case ConnectionState.none:
+              return const Text('loading always two...');
+            case ConnectionState.waiting:
+              return const Text('loading always three...');
+            default:
+              return const Text('loading not always...');
+          }
+        },
       ),
     );
   }
